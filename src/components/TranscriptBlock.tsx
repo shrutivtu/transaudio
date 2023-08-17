4590da91219e10a2587eafa33d0ba24572e64a23
shruti.saxena <shruti.saxena@ril.com>
2023-08-17 10:37:37 +0530

    chore: fix- styling of transcript layout

--- src/components/TranscriptBlock.tsx
+++ src/components/TranscriptBlock.tsx
@@ -1,16 +1,20 @@
 import { TranscriptType, TranscriptBlockType } from "../types";
 
-export const TranscriptBlock = ({ transcript } : { transcript: TranscriptType }) => {
-    return(<section>
-        {transcript && transcript.blocks.map((item: TranscriptBlockType, index: number) => {
+export const TranscriptBlock = ({
+  transcript,
+}: {
+  transcript: TranscriptType;
+}) => {
+  return (
+    <section className="transcript-blocks">
+      {transcript &&
+        transcript.blocks.map((item: TranscriptBlockType, index: number) => {
           return (
-            <p
-              key={index}
-              id={`id-${index}`}
-            >
+            <p key={index} id={`id-${index}`} className="transcript-block">
               {item.text}
             </p>
           );
         })}
-    </section>)
-}
+    </section>
+  );
+};
--- src/pages/Transcript.tsx
+++ src/pages/Transcript.tsx
@@ -5,27 +5,33 @@ import { TranscriptBlock, AudioPlayer } from "../components";
 import { TranscriptType } from "../types";
 
 export const Transcript = () => {
-    const { transcriptId } = useParams();
-    const [transcriptData, setTranscriptData] = useState<TranscriptType>({ id: "", title: "", audioUrl: "", blocks: [] });
-    async function fetchData() {
-        const data = await fetchTranscriptData(transcriptId);
-        setTranscriptData(data);
-    }
-    useEffect(() => {
-        fetchData()
-    }, [transcriptId])
-    
-    return (
-        <main>
-            <section>
-                <section>
-                    <h2>{transcriptData.title}</h2>
-                </section>
-                <TranscriptBlock transcript={transcriptData} />
-            </section>
-            <section>
-                <AudioPlayer />
-            </section>
-        </main>
-    );
-}
+  const { transcriptId } = useParams();
+  const [transcriptData, setTranscriptData] = useState<TranscriptType>({
+    id: "",
+    title: "",
+    audioUrl: "",
+    blocks: [],
+  });
+  async function fetchData() {
+    const data = await fetchTranscriptData(transcriptId);
+    setTranscriptData(data);
+  }
+  useEffect(() => {
+    fetchData();
+  }, [transcriptId]);
+
+  return (
+    <main className="transcript-root">
+      <section>
+        <div className="transcript-title">
+          <h2>{transcriptData.title}</h2>
+        </div>
+        {/* transcript text section */}
+        <TranscriptBlock transcript={transcriptData} />
+      </section>
+      <section>
+        <AudioPlayer />
+      </section>
+    </main>
+  );
+};
